59bde4ca27c63ea01b523dbe4d7bf9e2a52e78b5
Bogdan <Bogdanemails@gmail.com>
2020-11-17 12:45:59 +0200

    Добавили всі кнопки і функції до них(крім C, CE, backspase, -/+)

---
+++ Calkulator/Calkulator/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calkulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int num1;
+        int num2;
+        string op = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_num_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string str = button.Content.ToString();
+            int num = Int32.Parse(str);
+            if (op == "")
+            {
+                num1 = (num1 * 10) + num;
+                txtValue.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = num2 * 10 + num;
+                txtValue.Text = num2.ToString();
+            }
+
+        }
+
+
+        private void btn_operation_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string str = button.Content.ToString();
+            op = str;
+            txtValue.Text = op;
+        }
+
+        private void btn_Equal_Click(object sender, RoutedEventArgs e)
+        {
+            int result = 0;
+            switch (op)
+            {
+                case "+": result = num1 + num2; break;
+                case "-": result = num1 - num2; break;
+                case "*": result = num1 * num2; break;
+                case "/": result = num1 / num2; break;
+                case "min": result = Math.Min(num1, num2); break;
+                case "max": result = Math.Max(num1, num2); break;
+                case "avg": result = (num1 + num2) / 2; ; break;
+                case "x^y": result = Convert.ToInt32(Math.Pow(num1, num2)); break;
+                case "": result = num1 / num2; break;
+            }
+            txtValue.Text = result.ToString();
+            op = " ";
+            num1 = Int32.Parse(result.ToString());
+
+        }
+
+    }
+}
+
